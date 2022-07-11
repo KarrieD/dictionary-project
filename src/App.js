426c016bc3cca8c1d07e2e1a12dead8dd29bec50
KarrieD <karriedbrooks@gmail.com>
2022-07-11 17:25:21 -0500

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
+import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo}
-        className="App-logo"
-        alt='logo' />
-</header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Karrie Brooks</small>
+        </footer>
+      </div>
     </div>
   );
 }
